50436d13b06da284963732d045cfa1ff0399a566
Catalina Morales <cata@MacBook-Air-de-Catalina.local>
2020-11-05 00:23:55 -0300

    ahora si

---
+++ Primera_parte.m
@@ -0,0 +1,55 @@
+%Rango de valores
+x = 0:0.01:15*pi
+
+%Función 1
+
+f1 = 8*(log(4*x + 12)/log(5))
+
+%Función 2
+
+f2 = sin(6)*((log(x+9)/log(2))) + cos(7*(log(4*x+32)/log(6)))
+
+
+%Gráfico función 1
+plot(x,f1, 'r *')
+title('Gráfico función 1')
+xlabel('x')
+ylabel('a(x)')
+
+%Gráfico función 2
+plot(x,f2, 'g +')
+title('Gráfico función 2')
+xlabel('x')
+ylabel('b(x)')
+
+
+%Gráfico en conjunto
+
+hold on 
+
+plot(x,f1, 'r *')
+plot(x,f2, 'g +')
+title('Gráfico 1 y 2')
+xlabel('x')
+ylabel('y(x)')
+legend('a(x)','b(x)')
+
+hold off
+
+%Función 3
+
+x2 = -10:0.05:10
+f3 = 6*exp(x2+18)
+
+%Gráfico normal f3 
+plot(x2,f3, 'b .')
+title('Gráfico función 3')
+xlabel('x')
+ylabel('c(x)')
+
+
+%Gráfico función 3 escala logarítmica 
+semilogy(x2,f3, 'b .')
+title ( 'Gráfico función 3 en escala logarítmica' );
+xlabel('x')
+ylabel('c(x)')
---
+++ n_raphson.m
@@ -0,0 +1,15 @@
+function resultado = n_raphson(pol,p0,n,err)
+
+p = p0 - polyval(pol,p0)/polyval(polyder(pol),p0)
+e = abs((p - polyval(pol,p0))/polyval(polyder(pol),p))
+
+if n > 0 && err < e
+    n_raphson(pol,p,n-1,err)
+else
+    resultado = p0
+
+end
+
+end
+
+   
