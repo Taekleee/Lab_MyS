4b54d9f0b98f8c1336fb09b7497035f25a688fee
juan <juan.fernandez.m@usach.cl>
2021-01-10 01:34:39 -0300

    comentadas

--- Lab3/lab3_parte2.m
+++ Lab3/lab3_parte2.m
@@ -1,7 +1,21 @@
+
+%Función que calcula el modelo de estado de los vasos comunicantes
+%dado el area del primer vaso (a1), el area del segundo vaso (a2)
+%la resistencia de la salida del primer vaso (r1) y la resistencia
+%de la salida del segundo vaso (r2)
 function x = lab3_parte2(a1, a2, r1, r2)
+    %Se calcula la matriz A del modelo de estado
     A = [-1/(a1*r1), 1/(a1*r1);1/(a2*r1),  ((-1/(a2*r1)) - (1/(a2*r2)))];
+    
+    %Se calcula la matriz B del modelo de estado
     B = [1/a1; 0];
+    
+    %Se calcula la matriz C del modelo de estado
     C = [1, 0; 0, 1];
+    
+    %Se calcula la matriz D del modelo de estado
     D = [0;0];
+    
+    %Se genera el modelo de estado en función de las matrices A, B, C y D
     x = ss(A,B,C,D);
 end
--- Lab3/main_parte2.m
+++ Lab3/main_parte2.m
@@ -1,8 +1,22 @@
- H = lab3_parte2(2, 4, 0.25, 0.0625);
- t = linspace(0,12*pi,5000);
- u = 100*sin(t/4);
- u(u<0) = 0.;
- 
- step(H)
- impulse(H)
- lsim(H,u,t)
+%Se calcula el modelo de estado dada una entrada predeterminada.
+%a1 = 2
+%a2 = 4
+%r1 = 0.25
+%r2 = 0.0625
+H = lab3_parte2(2, 4, 0.25, 0.0625);
+
+%Se genera un conjunto de 5000 puntos t entre 0 a 12pi.
+t = linspace(0,12*pi,5000);
+%Se genera una función de entrada u sinusoidal con los puntos t
+u = 100*sin(t/4);
+%Se vuelve 0 todos los valores negativos de u
+u(u<0) = 0.;
+
+%se evalua el modelo de estado con un escalon
+step(H)
+
+%se evalua el modelo de estado con un impulso
+impulse(H)
+
+%se evalua el modelo de estado con la funcion u
+lsim(H,u,t)
