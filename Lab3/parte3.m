a921f67594d7912e78f28bd85cdb4ead19b67369
Catalina Morales <cata@MacBook-Air-de-Catalina.local>
2021-01-17 20:11:37 -0300

    primera parte

--- Lab3/parte3.m
+++ Lab3/parte3.m
@@ -1,3 +1,8 @@
+%Se llama a la función discretizar, variando el tiempo de muestreo y el
+%tipo de discretización que se quiera, en donde: 
+%zof = zero order hold
+%foh = first order hold
+
 discretizar(2, 'zoh')
 discretizar(2, 'foh')
 discretizar(0.1, 'zoh')
